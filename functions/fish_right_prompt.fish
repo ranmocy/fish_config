3f1d5d5fc946ae0e8b8e5b039e726f09c6c57482
Ranmocy <ranmocy@gmail.com>
2023-02-26 09:46:03 -0500

    Update

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,8 +1,16 @@
 function fish_right_prompt -d "Write out the right prompt"
+    set -l duration "$cmd_duration$CMD_DURATION"
+    if test $duration -gt 100
+        set duration (math $duration / 1000)s
+    else
+        set duration
+    end
+
     if [ $last_status != 0 ]
         printf (set_color -o red)$last_status
     end
+    printf (set_color -od green)$duration(set_color normal)
     printf (set_color -o blue)"("
-    printf (set_color yellow)(date +"%H:%M:%S %b %d")
+    printf (set_color -o yellow)(date +"%H:%M:%S %b %d")
     printf (set_color -o blue)")─┘ "(set_color normal)
 end
