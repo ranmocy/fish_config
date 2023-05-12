6b8e0f93d16af78a73dc200e6c48ad888e9714d1
Ranmocy <ranmocy@gmail.com>
2023-05-12 19:38:00 +0800

    Update

--- conf.d/00_path.fish
+++ conf.d/00_path.fish
@@ -3,6 +3,7 @@ if [ -x /opt/homebrew/bin/brew ]
     eval "$(/opt/homebrew/bin/brew shellenv)"
 end
 set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH ~/.foundry/bin $PATH
 set -gx PATH ~/.yarn/bin $PATH
