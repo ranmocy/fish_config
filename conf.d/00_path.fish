36725516578c1d0cafa4993e9485d97cb9b4bf23
Ranmocy <ranmocy@gmail.com>
2025-01-02 09:54:42 -0800

    Update

--- conf.d/00_path.fish
+++ conf.d/00_path.fish
@@ -2,9 +2,12 @@
 if [ -x /opt/homebrew/bin/brew ]
     eval "$(/opt/homebrew/bin/brew shellenv)"
 end
-set -gx PATH ~/bin $PATH
-set -gx PATH ~/.local/bin $PATH
-set -gx PATH ~/.cargo/bin $PATH
-set -gx PATH ~/.yarn/bin $PATH
+
+set -gx PATH $HOME/bin $HOME/.oh-my-configs/bin $PATH
+
+set -gx PATH $PATH $HOME/.local/bin
+set -gx PATH $PATH $HOME/.cargo/bin
+set -gx PATH $PATH $HOME/.yarn/bin
 set -gx PATH $PATH /Applications/WebStorm.app/Contents/MacOS
 set -gx PATH $PATH '/Applications/Visual Studio Code.app/Contents/Resources/app/bin'
+set -gx PATH $PATH $HOME/.cache/lm-studio/bin
