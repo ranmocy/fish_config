0e7a450c22b603cd86005bf7a300ddf4999b8bff
Ranmocy <ranmocy@gmail.com>
2025-01-17 01:39:06 -0800

    Update

--- conf.d/00_path.fish
+++ conf.d/00_path.fish
@@ -8,6 +8,7 @@ set -gx PATH $HOME/bin $HOME/.oh-my-configs/bin $PATH
 set -gx PATH $PATH $HOME/.local/bin
 set -gx PATH $PATH $HOME/.cargo/bin
 set -gx PATH $PATH $HOME/.yarn/bin
+set -gx PATH $PATH $HOME/Library/Python/3.9/bin
 set -gx PATH $PATH /Applications/WebStorm.app/Contents/MacOS
 set -gx PATH $PATH '/Applications/Visual Studio Code.app/Contents/Resources/app/bin'
 set -gx PATH $PATH $HOME/.cache/lm-studio/bin
